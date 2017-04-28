d63c3dfbf6fca99521e204e56a893d2ef43bf2ca
Maria Del Castillo <maria.f.delcastillo@gmail.com>
2017-04-28 16:04:06 -0700

    Finish JS Logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,26 @@
-$(function() {
+$(document).ready(function() {
   $("#track").submit(function(event) {
-    var appeal = $("input:radio[name=appeal]:checked").val();
-    var webpage-apps = $("input:radio[name=webpage-apps]:checked").val();
-    var painting-logic = $("input:radio[name=painting-logic]:checked").val();
-    var care-more = $("input:radio[name=care-more]:checked").val();
-    var left-right = $("input:radio[name=left-right]:checked").val();
+    var appeal = $("input[name=appeal]:checked").val();
+    var webpageApps = $("input[name=webpage-apps]:checked").val();
+    var paintingLogic = $("input[name=painting-logic]:checked").val();
+    var careMore = $("input[name=care-more]:checked").val();
+    var leftRight = $("input[name=left-right]:checked").val();
 
     if (appeal === "decorating") {
-    $("#css-design").show();
-    $("#java").hide();
-    $("#php").hide();
-  }
+      $("#css_design").show();
+      $("#java").hide();
+      $("#php").hide();
+    }
+    if (paintingLogic === "logic") {
+      $("#css_design").hide();
+      $("#java").show();
+      $("#php").hide();
+    }
+    if (careMore === "inner-workings") {
+      $("#css_design").hide();
+      $("#java").hide();
+      $("#php").show();
+    }
 
   event.preventDefault();
   });
