3e31acd9326c6b41e4b0f12228e2f11ebce9f40d
Maria Del Castillo <maria.f.delcastillo@gmail.com>
2017-05-16 22:47:23 -0700

    Assign numerical values to user input and rework if else according to values

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
+var add = function(appeal, webpageApps, paintingLogic, careMore, leftRight) {
+  return appeal + webpageApps + paintingLogic + careMore + leftRight;
+}
+
 $(document).ready(function() {
   $("#track").submit(function(event) {
-    var appeal = $("input[name=appeal]:checked").val();
-    var webpageApps = $("input[name=webpage-apps]:checked").val();
-    var paintingLogic = $("input[name=painting-logic]:checked").val();
-    var careMore = $("input[name=care-more]:checked").val();
-    var leftRight = $("input[name=left-right]:checked").val();
+    var appeal = parseInt($("input[name=appeal]:checked").val());
+    var webpageApps = parseInt($("input[name=webpage-apps]:checked").val());
+    var paintingLogic = parseInt($("input[name=painting-logic]:checked").val());
+    var careMore = parseInt($("input[name=care-more]:checked").val());
+    var leftRight = parseInt($("input[name=left-right]:checked").val());
+    var total = add(appeal, webpageApps, paintingLogic, careMore, leftRight);
+    
 
-    if (appeal === "decorating") {
-      $("#css_design").fadeIn(2000);
-      $("#java").hide();
-      $("#php").hide();
-    }
-    if (paintingLogic === "logic") {
-      $("#css_design").hide();
-      $("#java").fadeIn(2000);
-      $("#php").hide();
-    }
-    if (careMore === "inner-workings") {
-      $("#css_design").hide();
-      $("#java").hide();
+    if ( total <= 3 ) {
       $("#php").fadeIn(2000);
+    } else if ( total > 3 && total <= 5) {
+      $("#java").fadeIn(2000);
+    } else if ( total > 5 && total <=8) {
+      $("#css_design").fadeIn(2000);
     }
 
   event.preventDefault();
